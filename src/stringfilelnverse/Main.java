7615ae6fd82c6975c36bc7c5d419c209fa6c1234
Miroslav Genov <mgenov@gmail.com>
2011-06-17 13:54:50 +0300

    Minor formatting was being made in the stringfileInverse.Main

--- src/stringfilelnverse/Main.java
+++ src/stringfilelnverse/Main.java
@@ -3,17 +3,16 @@ package stringfilelnverse;
 import java.io.IOException;
 
 /**
- * Created by IntelliJ IDEA. User: adio
+ *
  */
 public class Main {
-    public static void main(String args[]){
+  public static void main(String args[]) {
 
-        FileReverser file = new FileReverser() ;
-        try{
-        file.fileReverse("Adio.txt");
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
+    FileReverser file = new FileReverser();
+    try {
+      file.fileReverse("Adio.txt");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
